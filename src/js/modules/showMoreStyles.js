91c493604a2ccee066c0ec2fdcb15b2c69e9ca60
Vladislav <des.kzv1318@gmail.com>
2022-02-26 11:50:16 +0300

    Loading elements from the server

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,42 @@
+import { getResource } from '../services/requests';
+
+
+const showMoreStyles = (trigger, wrapper) => {
+	const btn = document.querySelector(trigger);
+
+	btn.addEventListener('click', function() {
+		getResource('http://localhost:3000/syles')
+			.then(res => createCards(res))
+			.catch(() => errorMessage());
+
+		this.remove();
+	});
+
+	function errorMessage() {
+		let message = document.createElement('div');
+		message.classList.add('status');
+		message.textContent = 'Что-то пошло не так...';
+		message.style.cssText = 'font-size: 1.7rem; color: red; margin: 40px';
+		document.querySelector(wrapper).appendChild(message);
+	}
+
+	function createCards(response) {
+		response.forEach(({src, title, link}) => {
+			let card = document.createElement('div');
+
+			card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+			card.innerHTML = `
+				<div class="styles-block">
+					<img src=${src} alt=${title}>
+					<h4>${title}</h4>
+					<a href=${link}>Подробнее</a>
+				</div>
+			`;
+
+			document.querySelector(wrapper).appendChild(card);
+		})
+	}
+};
+
+export default showMoreStyles;
